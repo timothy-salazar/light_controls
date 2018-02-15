cc4cff28850cbd6a2f3cacacda7a62a614a3bf4e
Timothy Salazar <timothy.salazar@colorado.edu>
2018-02-15 13:42:39 -0700

    Update transmitRF.py
    Removed extraneous code

--- flask_app/light_controls/transmitRF.py
+++ flask_app/light_controls/transmitRF.py
@@ -53,61 +53,6 @@ def transmit_outlet(code):
     except KeyError:
         print('invalid input')
 
-
-def transmit_code(code):
-    '''Transmit a chosen code string using the GPIO transmitter'''
-    code_dict = {'a_on':'011101110101101111111010111',
-                'a_off':'101101110101101111111010111',
-                'b_on':'011101110101110111111010111',
-                'b_off':'101101110101110111111010111',
-                'c_on':'011101110101100111111010111',
-                'c_off':'101101110101100111111010111',
-                'd_on':'011110110101111111111010111',
-                'd_off':'101110110101111111111010111'}
-    try:
-        code = code_dict[code]
-        short_impulse = 0.0005
-        short_sleep = 0.0011
-        long_impulse = 0.00125
-        long_sleep = 0.00035
-        extended_delay = 0.01130
-        init_delay = 0.01125
-
-        NUM_ATTEMPTS = 4
-        NUM_REPS = 4
-        TRANSMIT_PIN = 27
-
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(TRANSMIT_PIN, GPIO.OUT)
-
-        for t in range(NUM_ATTEMPTS):
-            GPIO.output(TRANSMIT_PIN, 1)
-            time.sleep(short_impulse)
-            GPIO.output(TRANSMIT_PIN,0)
-            time.sleep(init_delay)
-            for r in range(NUM_REPS):
-                for i in code:
-                    if i == '1':
-                        GPIO.output(TRANSMIT_PIN, 1)
-                        time.sleep(short_impulse)
-                        GPIO.output(TRANSMIT_PIN, 0)
-                        time.sleep(short_sleep)
-                    elif i == '0':
-                        GPIO.output(TRANSMIT_PIN, 1)
-                        time.sleep(long_impulse)
-                        GPIO.output(TRANSMIT_PIN, 0)
-                        time.sleep(long_sleep)
-                    else:
-                        continue
-                GPIO.output(TRANSMIT_PIN, 0)
-                time.sleep(extended_delay)
-        GPIO.cleanup()
-    except KeyError:
-        print('invalid input')
-
-
-
-
 if __name__ == '__main__':
     for argument in sys.argv[1:]:
         exec('transmit_code(' + str(argument) + ')')
